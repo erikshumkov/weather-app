b5a39120c2669312bee133eb62a95503bc306c5f
Erik Shumkov <erikshumkov@gmail.com>
2019-10-13 17:22:30 +0200

    Updates

---
+++ precache-manifest.fd015311564abad55786bb7c75f45a5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf92626c43a886833114c621dfe58dc",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "066b78b488f6f8182f05",
+    "url": "/weather-app/static/css/main.6ebf79e5.chunk.css"
+  },
+  {
+    "revision": "f17258f6c4b3f7566429",
+    "url": "/weather-app/static/js/2.fbdc3d6a.chunk.js"
+  },
+  {
+    "revision": "066b78b488f6f8182f05",
+    "url": "/weather-app/static/js/main.7a8785cf.chunk.js"
+  },
+  {
+    "revision": "96d890d652711081467a",
+    "url": "/weather-app/static/js/runtime-main.aebf24a6.js"
+  }
+]);
